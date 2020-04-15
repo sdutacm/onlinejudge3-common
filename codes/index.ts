cdacd9ec0e1d6a742055bac28fe8f3f8a8d800d0
bLue <tbdblue@gmail.com>
2020-04-15 23:54:24 +0800

    U: codes

--- codes/index.ts
+++ codes/index.ts
@@ -1,5 +1,6 @@
 export const codeMsgs = {
-  // General 0XX
+  // General X
+  [-1]: 'Internal Server Error',
   1: 'Unknown Error',
   2: 'Illegal Request',
   3: 'You are not logged in',
@@ -10,12 +11,13 @@ export const codeMsgs = {
 };
 
 export enum Codes {
-  // General 0XX
-  R_GENERAL_UNKNOWN_ERROR = 1,
-  R_GENERAL_ILLEGAL_REQUEST = 2,
-  R_GENERAL_NOT_LOGGED_IN = 3,
-  R_GENERAL_ALREADY_LOGGED_IN = 4,
-  R_GENERAL_NO_PERMISSION = 5,
-  R_GENERAL_ENTITY_NOT_EXIST = 6,
-  R_GENERAL_FLE = 7,
+  // General X
+  GENERAL_INTERNAL_SERVER_ERROR = -1,
+  GENERAL_UNKNOWN_ERROR = 1,
+  GENERAL_ILLEGAL_REQUEST = 2,
+  GENERAL_NOT_LOGGED_IN = 3,
+  GENERAL_ALREADY_LOGGED_IN = 4,
+  GENERAL_NO_PERMISSION = 5,
+  GENERAL_ENTITY_NOT_EXIST = 6,
+  GENERAL_FLE = 7,
 }
