544dbf913ce2058759833b565f8e717fc7634f41
bLue <tbdblue@gmail.com>
2020-05-17 21:23:57 +0800

    A: contract

---
+++ contracts/user.req.ts
@@ -0,0 +1,14 @@
+/**
+ * This file was automatically generated.
+ * DO NOT MODIFY IT BY HAND. Instead, modify the source contract file,
+ * and run `npm run gen:contract` in "onlinejudge3-be" to regenerate this file.
+ */
+
+export interface IGetUserDetailReq {
+  userId: number;
+}
+
+export interface ILoginReq {
+  loginName: string;
+  password: string;
+}
