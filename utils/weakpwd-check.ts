167c82528401fa39fc22fe15760a9e061d057ff6
张晓硕 <1197219739@qq.com>
2023-11-13 23:29:31 +0800

    fix: re-define weak password

--- utils/weakpwd-check.ts
+++ utils/weakpwd-check.ts
@@ -1,27 +1,22 @@
 export default function isWeakPassword(password: string) {
-  // 检查密码长度
-  if (password.length < 8) {
+  // 检查密码长度: 最小长度为6
+  if (password.length < 6) {
     return true;
   }
 
-  // 检查字符组合
-  const hasUpperCase = /[A-Z]/.test(password);
-  const hasLowerCase = /[a-z]/.test(password);
+  // 检查字符组合: 需要存在字母、数字、特殊字符
+  const hasChar = /[A-Za-z]/.test(password);
   const hasNumber = /[0-9]/.test(password);
   const hasSpecialChar = /[^A-Za-z0-9]/.test(password);
 
-  if (!(hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar)) {
+  if (!(hasChar && hasNumber && hasSpecialChar)) {
     return true;
   }
 
-  // 检查字典匹配
-  const commonPasswords = ['123', 'abc']; // 常见密码列表
-  if (commonPasswords.includes(password.toLowerCase())) {
-    return true;
-  }
+  // 不允许出现三个以上的连续字母或者数字
+  const hasThreeConsecutiveChars = /([A-Za-z0-9])\1{2,}/.test(password);
 
-  // 检查重复字符
-  if (/(\w)\1{2,}/.test(password)) {
+  if (hasThreeConsecutiveChars) {
     return true;
   }
 
