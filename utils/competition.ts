c4ccf109f0723d20fc887f5f73e2138a03c133ef
bLue <tbdblue@gmail.com>
2023-12-15 13:10:09 +0800

    feat: add util compileVarScoreExpression

--- utils/competition.ts
+++ utils/competition.ts
@@ -5,9 +5,36 @@ import { ECompetitionUserRole } from '../enums';
  * @param roleExpr 要校验的角色表达式（或关系）
  * @param userRole 用户角色
  */
-export default function checkCompetitionUserRole(
+export function checkCompetitionUserRole(
   roleExpr: ECompetitionUserRole[],
   userRole: ECompetitionUserRole,
 ): boolean {
   return roleExpr.includes(userRole);
+}
+
+/**
+ * 编译题目可变分数表达式
+ * @param exp 表达式
+ * @param context 相关字段对象
+ */
+export function compileVarScoreExpression(
+  exp: string,
+  context: {
+    score: number;
+    problemIndex: number;
+    elapsedTime: {
+      h: number;
+      min: number;
+      s: number;
+    };
+    tries: number;
+  },
+): string {
+  return exp
+    .replace(/\$score/g, context.score.toString())
+    .replace(/\$problemIndex/g, context.problemIndex.toString())
+    .replace(/\$elapsedTime\.h/g, context.elapsedTime.h.toString())
+    .replace(/\$elapsedTime\.min/g, context.elapsedTime.min.toString())
+    .replace(/\$elapsedTime\.s/g, context.elapsedTime.s.toString())
+    .replace(/\$tries/g, context.tries.toString());
 }
